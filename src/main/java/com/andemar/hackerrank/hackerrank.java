db874447c96145ef175ae1b2b1d90884a25573ce
Anderson <andemartinezdev@gmail.com>
2023-08-13 18:56:05 -0500

    Java Stdin and Stdout I
    https://www.hackerrank.com/challenges/java-stdin-and-stdout-1/problem?isFullScreen=true

---
+++ src/main/java/com/andemar/hackerrank/hackerrank.java
@@ -0,0 +1,19 @@
+package com.andemar.hackerrank;
+
+import java.util.Scanner;
+
+public class hackerrank {
+
+  public static void main(String[] args) {
+
+    try (Scanner scanner = new Scanner(System.in)) {
+      int number;
+      while(scanner.hasNext()) {
+        System.out.println(scanner.nextInt());
+      }
+    }
+
+  }
+
+}
+
