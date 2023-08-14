f24c4f0784268a811d8977fa2ae977797d35f985
Anderson <andemartinezdev@gmail.com>
2023-08-13 19:08:48 -0500

    Java If-Else
    https://www.hackerrank.com/challenges/java-if-else/problem?isFullScreen=true

--- src/main/java/com/andemar/hackerrank/hackerrank.java
+++ src/main/java/com/andemar/hackerrank/hackerrank.java
@@ -1,19 +1,40 @@
 package com.andemar.hackerrank;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class hackerrank {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
 
-    try (Scanner scanner = new Scanner(System.in)) {
-      int number;
-      while(scanner.hasNext()) {
-        System.out.println(scanner.nextInt());
-      }
+    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    int number = Integer.parseInt(bufferedReader.readLine().trim());
+
+    if (isOdd(number)) {
+      System.out.println("Weird");
+
+    } else if(!isOdd(number) && inclusive(number, 2, 5)) {
+      System.out.println("Not Weird");
+
+    } else if(!isOdd(number) && inclusive(number, 6, 20)) {
+      System.out.println("Weird");
+
+    } else if(!isOdd(number) && inclusive(number, 21, 1000000)) {
+      System.out.println("Not Weird");
     }
 
+    bufferedReader.close();
   }
+
+  private static boolean inclusive(int number, int min, int max) {
+    return (number >= min) && (number <= max);
+  }
+
+  private static boolean isOdd(int number) {
+    return (number%2) != 0;
+  }
+
 
 }
 
